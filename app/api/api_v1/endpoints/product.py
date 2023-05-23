862c7dda50cca61b665c17982ead84377e266e87
Itzel Morales García <itzelmor02@ciencias.unam.mx>
2023-05-23 00:24:33 -0600

    Prueba michi

--- app/api/api_v1/endpoints/product.py
+++ app/api/api_v1/endpoints/product.py
@@ -21,10 +21,30 @@ logger = logging.getLogger(__name__)
 
 
 @router.get("/all", status_code=200, response_model=ProductSearchResults)
-def get_all_products() -> dict:
+def get_all_products(
+    *,
+    db: Session = Depends(deps.get_db),
+) -> dict:
     """
     Get all products, used by admin and vendedor
     """
-    return {"results": list()}
 
+    results = crud.product.get_multi(db=db)
+    if not results:
+        return {"results": []}
 
+    return {"results": results}
+
+#Este endpoint es para editar un producto, recibe un id y un objeto de tipo ProductUpdate
+@router.put("/{product_id}", status_code=200, response_model=ProductUpdate)
+def update_product(
+    product_id: int,
+    product: ProductUpdate,
+    db: Session = Depends(deps.get_db),
+    current_user: User = Depends(deps.get_current_active_user),
+) -> dict:
+    """
+    Update a product
+    """
+    if current_user.privilege == "admin":
+        return {"product": product}
