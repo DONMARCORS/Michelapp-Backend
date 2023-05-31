ccdc4b3cb7a1f67be1593e0905ae061252adde6a
Itzel Morales García <itzelmor02@ciencias.unam.mx>
2023-05-31 15:21:01 -0600

    Avance

--- app/api/api_v1/endpoints/product.py
+++ app/api/api_v1/endpoints/product.py
@@ -49,7 +49,36 @@ def update_product(
     if current_user.privilege == "admin":
         return {"product": product}
     
+    
+#Caso de uso: Crear un producto nuevo en la base de datos
+@router.post("/", status_code=201, response_model="Product Created")
+def create_product(
+    *,
+    product_id: int,
+    product: ProductUpdate,
+    db: Session = Depends(deps.get_db),
+    current_user: User = Depends(deps.get_current_user),
+        example={
+        "product_id": 123,
+        "name": "Clamato",
+        "description": "Jugo de tomate preparado con almejas y especias.",
+        "price": 32.5,
+        "stock": 34,
+        "image": "String",
+        }
+) -> dict:
+    """
+    Create a product. Only client cannot create products
+    """
+    if current_user.privilege != 3:
+        return {"product": product}
+    
+    if current_user.privilege == "vendedor":
+        raise HTTPException(status_code=404, detail=f"Product {item.product_id} not found",
+    if user_in.privilage != 1 and user_in.privilage != 2:
+        raise HTTPException(status_code=403, detail="Not authorized to perform this action")
 
+#Caso de uso: Eliminar un producto de la base de datos
 @router.delete("/{product_id}", status_code=200, response_model=ProductUpdate)
 def delete_product(
     product_id: int,
@@ -57,7 +86,7 @@ def delete_product(
     db: Session = Depends(deps.get_db),
 ) -> dict:
     """
-    Delete a product
+    Delete a product. Only client cannot delete products
     """
     if current_user.privilege == "admin":
         return {"product": product}
@@ -65,17 +94,4 @@ def delete_product(
     raise HTTPException(
         status_code=404,
         detail=f"Product {item.product_id} not found",
-    )
-    
-
-@router.post("/", status_code=201, response_model=ProductUpdate)
-def create_product(
-    product_id: int,
-    product: ProductUpdate,
-    db: Session = Depends(deps.get_db),
-) -> dict:
-    """
-    Create a product
-    """
-    if current_user.privilege == "admin":
-        return {"product": product}
+    )|
