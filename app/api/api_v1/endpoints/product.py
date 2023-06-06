1597a53beb45744a760f9a64a0f78e24fc8ae679
itzelmor02 <itzelmor02@ciencias.unam.mx>
2023-06-05 18:40:17 -0600

    products fix

--- app/api/api_v1/endpoints/product.py
+++ app/api/api_v1/endpoints/product.py
@@ -1,13 +1,13 @@
 import logging
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Body, Depends, HTTPException
 
 from sqlalchemy.orm import Session
 from app import crud
 from app.api import deps
 
 from app.models.user import User
-from app.schemas.product import ProductUpdate
+from app.schemas.product import ProductUpdate, ProductCreate, Product
 
 
 
@@ -18,6 +18,11 @@ from app.schemas.product import (
 router = APIRouter()
 
 logger = logging.getLogger(__name__)
+
+authorization_exception = HTTPException(
+        status_code=403,
+        detail="Not authorized to perform this action",
+    )
 
 
 #Caso de uso: Obtener todos los productos de la base de datos
@@ -37,65 +42,85 @@ def get_products(
     return {"results": list(results)}
 
 #Caso de uso: Crear un producto nuevo en la base de datos
-@router.post("/", status_code=201, response_model="Product Created")
+@router.post("/", status_code=201, response_model=Product, response_description="Product created")
 def create_product(
     *,
-    product_id: int,
-    product: ProductUpdate,
-    db: Session = Depends(deps.get_db),
-    current_user: User = Depends(deps.get_current_user),
+    product: ProductCreate =  Body(
+        ...,
         example={
-        "product_id": 123,
+        "id": 123,
         "name": "Clamato",
+        "quantity": 15,
         "description": "Jugo de tomate preparado con almejas y especias.",
         "price": 32.5,
-        "stock": 34,
-        "image": "String",
         }
-) -> dict:
+    ),
+    
+    db: Session = Depends(deps.get_db),
+    current_user: User = Depends(deps.get_current_user),
+        
+) -> Product:
     """
     Create a product. Only vendedor can create products
     """
-    if current_user.privilege == 2:
-        return {"product": product}
+    logger.debug(current_user.privilege)
+    if current_user.privilege != 2 and current_user.privilege != 1:
+        raise authorization_exception
     
-    if current_user.privilege == "vendedor":
-        raise HTTPException(status_code=404, detail=f"Product {item.product_id} not found",
-    if user_in.privilage != 1 and user_in.privilage != 2:
-        raise HTTPException(status_code=403, detail="Not authorized to perform this action")
+    return crud.product.create(db=db, obj_in=product)
+
+
 
 #Caso de uso: Eliminar un producto de la base de datos
-@router.delete("/{product_id}", status_code=200, response_model=ProductUpdate)
+@router.delete("/{product_id}", status_code=200)
 def delete_product(
+    *,
     product_id: int,
-    product: ProductUpdate,
+    current_user: User = Depends(deps.get_current_user),
     db: Session = Depends(deps.get_db),
 ) -> dict:
     """
     Delete a product. Only vendedor can delete products
     """
-    if current_user.privilege == 2:
-        return {"product": product}
+    if current_user.privilege != 2 and current_user.privilege != 1:
+        raise authorization_exception
     
-    raise HTTPException(
-        status_code=404,
-        detail=f"Product {item.product_id} not found",
-    )|
+    crud.product.remove(db=db, id=product_id)
+
+    return {"message": "Product deleted"}
     
 #Caso de uso: Actualizar un producto de la base de datos
-@router.put("/{product_id}", status_code=200, response_model=ProductUpdate)
+@router.put("/{product_id}", status_code=200, response_model=Product)
 def update_product(
+    *,
     product_id: int,
-    product: ProductUpdate,
+    product_in: ProductUpdate = Body(
+        ...,
+        example={
+        "id": 123,
+        "name": "Clamato",
+        "quantity": 15,
+        "description": "Jugo de tomate preparado con almejas y especias.",
+        "price": 32.5,
+        }
+    ),
+    current_user: User = Depends(deps.get_current_user),
     db: Session = Depends(deps.get_db),
-) -> dict:
+) -> Product:
     """
     Update a product. Only client cannot update products
     """
-    if current_user.privilege == "admin":
-        return {"product": product}
+    if current_user.privilege != 2 and current_user.privilege != 1:
+        raise authorization_exception
     
-    raise HTTPException(
-        status_code=404,
-        detail=f"Product {item.product_id} not found",
-    )
+    product = crud.product.get(db=db, id=product_id)
+
+    if not product:
+        raise HTTPException(
+            status_code=404,
+            detail="Product not found",
+        )
+    
+    product = crud.product.update(db=db, db_obj=product, obj_in=product_in)
+    return product
+    
